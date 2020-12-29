01f474a129778ffba65213211da1241e40767e9c
Chip Camden <eckhart.csjc@gmail.com>
2020-12-29 16:40:37 -0500

    add and update methods for composers

--- src/composer.ts
+++ src/composer.ts
@@ -11,23 +11,66 @@ export interface IComposer {
   died?: string;      // Valid dayjs input, or undefined is still living
 }
 
+export interface IComposerUpdater extends Partial<IComposer> {
+  name: string;
+}
+
 export interface IComposerStats {
   nComposers: number;
 }
 
 const composerFile = new ArrayFileHandler<IComposer>('composers.json');
 
-const indexComposers = () => composerFile.fetch().reduce<Record<string,IComposer>>((accum, composer) => ({
-   ...accum,
-   [composer.name]: composer,
-   ...composer.aliases.reduce<Record<string,IComposer>>((ac, alias) => ({ ...ac, [alias]: composer }), {} as Record<string,IComposer>),
-}), {} as Record<string,IComposer>);
+const indexComposers = (composers?: IComposer[]) => 
+  (composers ?? composerFile.fetch())
+    .reduce<Record<string,IComposer>>((accum, composer) => ({
+       ...accum,
+       [composer.name]: composer,
+       ...composer.aliases.reduce<Record<string,IComposer>>((ac, alias) => ({ ...ac, [alias]: composer }), {} as Record<string,IComposer>),
+    }), {} as Record<string,IComposer>);
 
-export const find = (name: string) => indexComposers()[name];
+export const find = (name: string, composers?: IComposer[]) => indexComposers(composers)[name];
 export const suggest = (name: string) => _.sortBy(composerFile.fetch().map((composer:IComposer) => ({
   distance: [ levenshtein(composer.name, name), ...composer.aliases.map((alias) => levenshtein(alias,name))].sort()[0],
   composer,
 })), ['distance']);
+
+const checkAliases = (composer: IComposer, composers: IComposer[]) => {
+  const index = indexComposers(composers);
+  return composer.aliases.map<IComposer | undefined>((alias) => index[alias]).filter((c) => !!c && c !== composer);
+}
+  
+export const add = (composer: IComposer) => {
+  const composers = composerFile.fetch();
+  const existing = find(composer.name, composers);
+  if (existing) {
+    console.error(`Composer ${composer.name} already exists`, existing);
+    return;
+  }
+  const existingAliases = checkAliases(composer, composers);
+  if (existingAliases.length > 0) {
+    console.error(`The following aliases for this composer already exist`, existingAliases);
+    return;
+  }
+  composerFile.save([...composers, composer]);
+};
+
+export const update = (updates: IComposerUpdater) => {
+  const composers = composerFile.fetch();
+  const existing = find(updates.name, composers);
+  if (!existing) {
+    console.error(`No composer named ${updates.name} found to update`);
+  }
+  _.merge(existing, updates);
+  if (updates.aliases) {
+    const existingAliases = checkAliases(existing, composers);
+    if (existingAliases.length > 0) {
+      console.error(`The following aliases for this composer already exist`, existingAliases);
+      return;
+    }
+  }
+  composerFile.save(composers);
+};
 
 export const stats = (): IComposerStats => {
   const composers = composerFile.fetch();
