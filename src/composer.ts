bd250125e0e5745b1ff3cbd4640d7ff183b94306
Chip Camden <eckhart.csjc@gmail.com>
2021-02-01 13:59:00 -0500

    info doesn't need to duplicate composerKey

--- src/composer.ts
+++ src/composer.ts
@@ -267,7 +267,7 @@ export const formatInfo = (composer?: string[], composerKey?: string) => {
   const c = composerKey ? find(composerKey) : undefined;
   const aliases = [
     (c?.name !== name) ? c?.name : undefined,
-    ...(c?.aliases ?? []),
+    ...(c?.aliases.filter((a) => a !== composerKey) ?? []),
   ].filter((a) => !!a);
   return `Composer: ${name}${aliases.length > 0 ? ' (' + aliases.join(', ') + ')' : ''}, born: ${c?.born ?? '?'}${c?.died ? ' died: ' + c?.died : ''}`;
 }
