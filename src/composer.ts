6a7f6669770305a27269f1019f2d6a613af2849d
Chip Camden <eckhart.csjc@gmail.com>
2021-02-21 16:52:15 -0500

    remove irrelevant aliases from info

--- src/composer.ts
+++ src/composer.ts
@@ -223,9 +223,8 @@ export const resolve = async (name: string, tracks: track.ITrack[]): Promise<boo
 export const formatInfo = (composer?: string[], composerKey?: string) => {
   const name = composer?.join(' & ') ?? composerKey ?? '?';
   const c = composerKey ? find(composerKey) : undefined;
-  const aliases = [
-    (c?.name !== name) ? c?.name : undefined,
-    ...(c?.aliases.filter((a) => a !== composerKey) ?? []),
-  ].filter((a) => !!a);
-  return `Composer: ${name}${aliases.length > 0 ? ' (' + aliases.join(', ') + ')' : ''}, born: ${c?.born ?? '?'}${c?.died ? ' died: ' + c?.died : ''}`;
+  const alias = (composerKey && composerKey !== name) ?
+    ` (${composerKey})` :
+    '';
+  return `Composer: ${name}${alias}, born: ${c?.born ?? '?'}${c?.died ? ' died: ' + c?.died : ''}`;
 }
