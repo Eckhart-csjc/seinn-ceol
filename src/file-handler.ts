5cf9894cf67db45a21d9ef41a5410d6c9f03b95f
Chip Camden <eckhart.csjc@gmail.com>
2021-05-02 16:29:23 -0400

    cache file reads

--- src/file-handler.ts
+++ src/file-handler.ts
@@ -5,30 +5,40 @@ import { error } from './util';
 
 export class FileHandler<T> {
 
+  filename: string;
+  cache: T | undefined;
+  mtime: number;
+
   constructor (public baseFilename: string, public pathOverride?: string) {
+    this.filename = this.makeFilename(this.baseFilename, this.pathOverride);
+    this.cache = undefined;
+    this.mtime = 0;
   }
 
   public fetch(): T | undefined {
-    const filename = this.makeFilename(this.baseFilename, this.pathOverride);
-    if (fs.existsSync(filename)) {
-      try {
-        return JSON.parse(fs.readFileSync(filename, { encoding: 'utf8' }).normalize()) as T;
-      } catch (e) {
-        error(`Error reading and parsing file ${filename}: ${e.message}`);
-        process.exit(1);
+    if (fs.existsSync(this.filename)) {
+      const stat = fs.statSync(this.filename);
+      if (this.mtime < stat.mtimeMs) {
+        try {
+          this.cache = JSON.parse(fs.readFileSync(this.filename, { encoding: 'utf8' }).normalize()) as T;
+          this.mtime = stat.mtimeMs;
+        } catch (e) {
+          error(`Error reading and parsing file ${this.filename}: ${e.message}`);
+          process.exit(1);
+        }
       }
+      return this.cache;
     }
     return undefined;
   }
 
   public save(data: T) {
-    const filename = this.makeFilename(this.baseFilename, this.pathOverride);
-    const tmpFilename = `${filename}.tmp`;
+    const tmpFilename = `${this.filename}.tmp`;
     if (fs.existsSync(tmpFilename)) {
       fs.rmSync(tmpFilename);
     }
     fs.appendFileSync(tmpFilename, JSON.stringify(data, undefined, 2), { encoding: 'utf8' });
-    fs.renameSync(tmpFilename, filename);
+    fs.renameSync(tmpFilename, this.filename);
   };
 
   private getDefaultPath() {
